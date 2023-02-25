986751c5f4b77e8b838ed59d1947e5793c980fb2
Dave Pearson <davep@davep.org>
2023-02-25 09:49:57 +0000

    :sparkles: Improve block loading

--- twobee/lib/sequence.py
+++ twobee/lib/sequence.py
@@ -1,12 +1,29 @@
 """Code for reading and handling a DNA sequence in a 2bit file."""
 
 ##############################################################################
+# Python imports.
+from __future__  import annotations
+from dataclasses import dataclass
+
+##############################################################################
 # Rich imports.
 from rich.repr import Result
 
 ##############################################################################
 # Local imports.
 from .reader_protocol import TwoBitReaderInterface
+
+##############################################################################
+# Class that holds the start and end of a block.
+@dataclass
+class TwoBitBlock:
+
+    start: int
+    """The start location of the block (inclusive)."""
+    end: int
+    """The end location of the block (exclusive)."""
+    size: int
+    """The size of the block."""
 
 ##############################################################################
 class TwoBitSequence:
@@ -33,14 +50,10 @@ class TwoBitSequence:
         self._dna_size = reader.read_long()
 
         # Get the N block data.
-        self._n_block_counts = reader.read_long()
-        self._n_block_starts = reader.read_long_array( self._n_block_counts )
-        self._n_block_sizes  = reader.read_long_array( self._n_block_counts )
+        self.n_blocks = self._load_blocks()
 
         # Get the mask block data.
-        self._mask_block_counts = reader.read_long()
-        self._mask_block_starts = reader.read_long_array( self._mask_block_counts )
-        self._mask_block_sizes  = reader.read_long_array( self._mask_block_counts )
+        self.mask_blocks = self._load_blocks()
 
         # We should now be on the reserved long integer. It should always be
         # zero.
@@ -51,11 +64,24 @@ class TwoBitSequence:
         # that.
         self._dna_start = self._reader.position()
 
+    def _load_blocks( self ) -> tuple[ TwoBitBlock, ... ]:
+        """Load the block data at the current location.
+
+        Returns:
+            A tuple of the blocks read.
+        """
+        counts = self._reader.read_long()
+        starts = self._reader.read_long_array( counts )
+        sizes  = self._reader.read_long_array( counts )
+        return tuple( TwoBitBlock( start, start + size, size ) for start, size in zip( starts, sizes ) )
+
     def __rich_repr__( self ) -> Result:
         """Make the object look nice in Rich."""
         yield self._name
         yield "offset", self._offset
         yield "dna_size", self._dna_size
+        yield "len(n_blocks)", len( self.n_blocks )
+        yield "len(mask_blocks)", len( self.mask_blocks )
 
     @property
     def name( self ) -> str:
