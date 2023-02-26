014c29f2d9086b6959ca4538a36109173bbe9faf
Dave Pearson <davep@davep.org>
2023-02-26 10:25:07 +0000

    :sparkles: Add some initial layout to the main screen

--- twobee/chui/app.py
+++ twobee/chui/app.py
@@ -1,7 +1,14 @@
 """Defines the application class for TwoBee."""
 
 ##############################################################################
+# Python imports.
+from __future__ import annotations
+from argparse   import Namespace, ArgumentParser, ArgumentTypeError
+from pathlib    import Path
+
+##############################################################################
 # Textual imports/
+from textual     import __version__ as textual_version
 from textual.app import App
 
 ##############################################################################
@@ -19,13 +26,67 @@ class TwoBeeApp( App[ None ] ):
     SUB_TITLE = f"A simple 2bit viewer for the terminal - v{__version__}"
     """The sub-title for the application."""
 
+    def __init__( self, cli_args: Namespace ) -> None:
+        """Initialise the app."""
+        super().__init__()
+        self._args = cli_args
+
     def on_mount( self ) -> None:
         """Configure the application once the DOM is up and running."""
-        self.push_screen( Main() )
+        self.push_screen( Main( self._args.file ) )
+
+##############################################################################
+def py_file( path: str ) -> Path:
+    """Check that the file we're being asked to look at seems fine
+
+    Args:
+        path: The argument.
+
+    Returns:
+        The `Path` to the file if it looks okay.
+    """
+    candidate = Path( path )
+    if not candidate.exists():
+        raise ArgumentTypeError( f"{path} does not exist" )
+    return candidate
+
+##############################################################################
+def get_args() -> Namespace:
+    """Parse and return the command line arguments.
+
+    Returns:
+        The result of parsing the arguments.
+    """
+
+    # Create the parser object.
+    parser = ArgumentParser(
+        prog        = "twobee",
+        description = "A simple terminal-based 2bit file viewer.",
+        epilog      = f"v{__version__}"
+    )
+
+    # Add --version
+    parser.add_argument(
+        "-v", "--version",
+        help    = "Show version information.",
+        action  = "version",
+        version = f"%(prog)s {__version__} (Textual v{textual_version})"
+    )
+
+    # The remainder is the file to view.
+    parser.add_argument(
+        "file",
+        help    = "The 2bit file to view",
+        type    = py_file,
+        default = "."
+    )
+
+    # Finally, parse the command line.
+    return parser.parse_args()
 
 ##############################################################################
 def run() -> None:
     """Run the application."""
-    TwoBeeApp().run()
+    TwoBeeApp( get_args() ).run()
 
 ### app.py ends here
--- twobee/chui/screens/main.py
+++ twobee/chui/screens/main.py
@@ -1,18 +1,51 @@
 """The main screen for the TwoBee application."""
 
 ##############################################################################
+# Python imports.
+from pathlib import Path
+
+##############################################################################
 # Textual imports.
-from textual.app     import ComposeResult
-from textual.screen  import Screen
-from textual.widgets import Header, Footer
+from textual.app        import ComposeResult
+from textual.screen     import Screen
+from textual.containers import Horizontal
+from textual.widgets    import Header, Footer, Tree, Static
+
+##############################################################################
+# Local imports.
+from twobee import TwoBitFileReader
 
 ##############################################################################
 class Main( Screen ):
     """The main screen for the TwoBee application."""
 
+    DEFAULT_CSS = """
+    Tree {
+        width: 25%;
+        border-right: vkey $panel-lighten-2;
+    }
+    """
+
+    def __init__( self, file: Path ) -> None:
+        """Initialise the main screen."""
+        super().__init__()
+        self._file = file
+        self._reader = TwoBitFileReader( str( file ) )
+
     def compose( self ) -> ComposeResult:
         """Compose the main screen of the application."""
         yield Header()
+        with Horizontal():
+            yield Tree[ str ]( str( self._file.stem ) )
+            yield Static( "TODO: data goes here" )
         yield Footer()
+
+    def on_mount( self ) -> None:
+        """Populate the screen once the DOM is up and running."""
+        file_map = self.query_one( Tree )
+        for chromosome in self._reader:
+            file_map.root.add_leaf( chromosome, data=chromosome )
+        file_map.root.expand()
+        file_map.focus()
 
 ### main.py ends here
