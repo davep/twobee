a818b4f85509ec5b1c01354d9a51224b343b5d19
Dave Pearson <davep@davep.org>
2023-03-02 21:41:45 +0000

    :rocket: Add initial packaging information

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup file for the twobee application."""
+
+##############################################################################
+# Python imports.
+from setuptools import setup
+
+##############################################################################
+# Perform the setup.
+if __name__ == "__main__":
+    setup()
+
+### setup.py ends here
